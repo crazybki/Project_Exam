9bcb93c4cfd13cadab822b53f7fb60e476f131a7
bki123 <41273107+crazybki@users.noreply.github.com>
2020-01-13 19:12:34 +0100

    First commit, uploaded folders for html, CSS and JS

---
+++ js/spaceXJS.js
@@ -0,0 +1,12 @@
+const hobbies = ['poker', 'fotball', 'handball'];
+
+for (var i = 0; i < hobbies.length; i++) {
+    console.log(hobbies);
+}
+
+function someThing (a,b) {
+   var sum = a+b;
+    return sum;
+} 
+
+console.log(someThing(2,2));
