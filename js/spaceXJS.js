a4c96c943e1a9f7d6014bb0b9696aa8177908354
bki123 <41273107+crazybki@users.noreply.github.com>
2020-01-17 00:40:22 +0100

    added html and css

--- js/spaceXJS.js
+++ js/spaceXJS.js
@@ -1,12 +1,5 @@
-const hobbies = ['poker', 'fotball', 'handball'];
+var a = 2;
+var b;
 
-for (var i = 0; i < hobbies.length; i++) {
-    console.log(hobbies);
-}
 
-function someThing (a,b) {
-   var sum = a+b;
-    return sum;
-} 
-
-console.log(someThing(2,2));
+console.log('I work, please check the console and send cameron a screen shot')
