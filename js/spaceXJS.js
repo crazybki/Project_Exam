6b46e7a9d26ed91eade07c4b63001c50c36831f1
bki123 <41273107+crazybki@users.noreply.github.com>
2020-01-21 22:31:58 +0100

    added counter

---
+++ js/spaceXJS.js
@@ -0,0 +1,29 @@
+
+//Counter on frontpage
+
+var launchday = new Date('Mar 1, 2020 23:59:59').getTime();
+console.log(launchday)
+
+var countDown = setInterval(function() {
+    var thisDay = new Date().getTime();
+    var timeToLaunch = launchday - thisDay;
+
+    var dayOfLaunch = Math.floor(timeToLaunch / (1000 * 60 * 60 * 24));
+    var hourOfLaunch = Math.floor((timeToLaunch % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutesOfLaunch = Math.floor((timeToLaunch % (1000 * 60 * 60)) / (1000 * 60));
+    var secondsOfLaunch = Math.floor((timeToLaunch % (1000 * 60)) / 1000);
+
+    var dayElement = document.getElementById('day_clock');
+    dayElement.innerHTML = dayOfLaunch;
+    var hourElement = document.getElementById('hour_clock');
+    hourElement.innerHTML = ('0' + hourOfLaunch).slice(-2);
+    var minutesElement = document.getElementById('minutes_clock');
+    minutesElement.innerHTML = ('0' + minutesOfLaunch).slice(-2);
+    var secondsElement = document.getElementById('seconds_clock');
+    secondsElement.innerHTML = ('0' + secondsOfLaunch).slice(-2);
+
+    if (timeToLaunch < 0) {
+        clearInterval(countDown); 
+        document.getElementById('launch_paragraph').innerHTML = "Liftoff!!";
+    }
+}, 1000);
