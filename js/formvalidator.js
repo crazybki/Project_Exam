3641923ca5712ab5901a46bf87997bc42097cb52
bki123 <41273107+crazybki@users.noreply.github.com>
2020-02-03 22:48:27 +0100

    added js and css in about html

--- js/formvalidator.js
+++ js/formvalidator.js
@@ -1,2 +1,21 @@
-var submitBtn = document.getElementById('input[type="submit"]');
-submitBtn.addEventListener('click', submitName);
+
+var submitBtn = document.getElementById('send');
+
+submitBtn.addEventListener('focus', submitName);
+
+function submitName () {
+    var checkFirstName = /^[a-zA-Z ]+$/;
+    var inputfirstName = checkFirstName.test(document.getElementById('firstname').value);
+    var styleTxtField = document.getElementById('firstname');
+    if (inputfirstName === true) {
+        event.preventDefault();
+        styleTxtField.style.backgroundColor = 'green';
+        console.log('correct')
+        
+    } else {
+        event.preventDefault();
+        styleTxtField.style.backgroundColor = 'red';
+        console.log('wrong');
+        
+    }
+}
