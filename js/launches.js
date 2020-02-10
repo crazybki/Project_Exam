613f2e0eb1acd86c124363baf85862f1a42b6d39
bki123 <41273107+crazybki@users.noreply.github.com>
2020-02-10 20:21:04 +0100

    added js file to launch page and connected to api

---
+++ js/launches.js
@@ -0,0 +1,29 @@
+//Fetch API past launches
+
+fetch ("https://api.spacexdata.com/v3/launches/past/?limit=15&offset=74")
+  .then(result => result.json())
+    .then ((res) => {
+        pastLaunches(res);
+    })
+  .catch(err => console.log(err))
+
+function pastLaunches (result) {
+    var getLaunches = document.getElementById('launches_flex');
+    
+    for (var i = 0; i < result.length; i++){
+        var nameOfLaunch = document.createElement('h2');
+        nameOfLaunch.innerHTML = result[i].mission_name;
+        nameOfLaunch.classList = 'launch_heading';
+        getLaunches.appendChild(nameOfLaunch);
+
+        var videoLink = document.createElement('iframe');
+        videoLink.classList = 'launch_links'
+        videoLink.src = "https://www.youtube.com/embed/" + result[i].links.youtube_id;
+        getLaunches.appendChild(videoLink);
+
+        var launchInfo = document.createElement('p');
+        launchInfo.classList = 'launch_info'
+        launchInfo.innerHTML = result[i].details;
+        getLaunches.appendChild(launchInfo)
+    }
+};
